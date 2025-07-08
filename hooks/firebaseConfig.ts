930720c171d04c8a8004939b961f5f2c1b77e0da
sneha-jhaveri <sneha@botwot.io>
2025-07-08 22:33:54 +0530

    update

--- hooks/firebaseConfig.ts
+++ hooks/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyAlWu5-bcN5B6RvCxVrRv4q-IVZorZWGd0',
-    authDomain: 'kai-db-b661d.firebaseapp.com',
-    projectId: 'kai-db-b661d',
-    storageBucket: 'kai-db-b661d.firebasestorage.app',
-    messagingSenderId: '1050715964408',
-    appId: '1:1050715964408:web:fff5d338659b4b41804c0d',
-    measurementId: 'G-T1ZFHWS82Y',
+  apiKey: 'AIzaSyAlWu5-bcN5B6RvCxVrRv4q-IVZorZWGd0',
+  authDomain: 'kai-db-b661d.firebaseapp.com',
+  projectId: 'kai-db-b661d',
+  storageBucket: 'kai-db-b661d.firebasestorage.app',
+  messagingSenderId: '1050715964408',
+  appId: '1:1050715964408:web:fff5d338659b4b41804c0d',
+  measurementId: 'G-T1ZFHWS82Y',
 };
 
 // 🛡️ Prevent reinitialization
